c329b630593e5e13356174ee6a3f63d68614ef9f
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-06 21:55:35 +0530

    Add API routes for video management.

---
+++ app/api/videos/route.ts
@@ -0,0 +1,66 @@
+import { authOptions } from "@/lib/auth";
+import { connectToDatabase } from "@/lib/db";
+import Video, { IVideo } from "@/models/Video";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+// get all videos
+export async function GET() {
+  try {
+    await connectToDatabase();
+    const videos = await Video.find({}).sort({ createdAt: -1 }).lean();
+    if (!videos || videos.length === 0) {
+      return NextResponse.json([], { status: 200 });
+    }
+    return NextResponse.json(videos);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to fetch videos" },
+      { status: 400 }
+    );
+  }
+}
+
+// upload a video
+export async function POST(request: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session)
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    await connectToDatabase();
+
+    const body: IVideo = await request.json();
+    if (
+      !body.title ||
+      !body.desciption ||
+      !body.videoUrl ||
+      !body.thumbnailUrl
+    ) {
+      return NextResponse.json(
+        { error: "Fields are missing." },
+        { status: 400 }
+      );
+    }
+
+    const videoData = {
+      ...body,
+      controls: body.controls ?? true,
+      transformation: {
+        height: 1920,
+        width: 1080,
+        quality: body.transformation?.quality ?? 100,
+      },
+    };
+
+    const newVideo = await Video.create({ videoData });
+    return NextResponse.json(newVideo);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Failed to create a video." },
+      { status: 400 }
+    );
+  }
+}
