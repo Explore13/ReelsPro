c32d3d0ad483f4dfc7d2c871eebfff241a6ff3ce
Explore13 <suryaghoshchemistry2k21@gmail.com>
2025-02-08 01:32:51 +0530

    Add Header component and registration page with form handling

---
+++ app/register/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Your password does not match.");
+    }
+
+    try {
+      const res = await fetch("/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!res.ok) setError("Registration Error");
+      const data = res.json();
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+      setError("Error during registration");
+    }
+  };
+
+  return <div>Register</div>;
+}
+
+export default Register;
